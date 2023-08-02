c1f013bec4ed5e44f410df45d35418dca9d31590
DoormatIka <trashcanartistlily@gmail.com>
2023-08-03 02:01:06 +0900

    last_commit

--- index.ts
+++ index.ts
@@ -11,12 +11,14 @@ import { select } from "./src/format_operators.js";
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // quite dangerous ay?
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
+let interval: NodeJS.Timer;
+
 const app = express();
 app.use(express.json());
 app.listen(3000, () => {
     console.log("Listening to port 3000.");
 });
-
+const folderID = "18Dtd9oz8HrgDiixqC18jU9eufy75d22l";
 const oauth2Client = new googledrive.auth.OAuth2(json.clientID, json.clientSecret, json.redirectURI)
 oauth2Client.setCredentials({ refresh_token: json.token })
 const drive = googledrive.drive({
@@ -40,9 +42,9 @@ app.post("/download", async (req, res) => {
         const mimetype = mime.lookup(yt_info._filename);
         const response = await drive.files.create({
             requestBody: {
-                name: yt_info._filename,
+                name: path.basename(yt_info._filename),
                 mimeType: mimetype ? mimetype : "",
-                parents: ["18Dtd9oz8HrgDiixqC18jU9eufy75d22l"]
+                parents: [folderID]
             },
             media: {
                 mimeType: mimetype ? mimetype : "",
@@ -61,7 +63,7 @@ app.post("/download", async (req, res) => {
             fields: "name,webViewLink,webContentLink"
         })
         res.send({
-            name: path.basename(file_get.data.name!),
+            name: file_get.data.name!,
             view: file_get.data.webViewLink,
             content: file_get.data.webContentLink,
             ...{
@@ -76,16 +78,32 @@ app.post("/download", async (req, res) => {
 app.get("/listall", async (req, res) => {
     const list = await drive.files.list({
         fields: "files(name,createdTime,webContentLink)",
+        q: `'${folderID}' in parents`
     });
     res.send(list.data);
 });
 
-const files: { createdTimeMS: number, id: string, endingTimeMS: number }[] = [];
-// setInterval(fi, 1800000);
+app.post("/enableAutoDeletion", async (req, res) => {
+    const minutes = req.body.minutes * 60 * 1000 ?? 60 * 60 * 1000;
+    interval = setInterval(() => {
+        initialize(minutes);
+    }, Math.floor(minutes / 2));
+    res.send(`Enabled auto deletion for ${minutes}ms.`);
+});
+app.post("/disableAutoDeletion", async (req, res) => {
+    clearInterval(interval);
+    res.send(`Disabled auto deletion.`)
+})
 
-async function initialize() {
+async function initialize(minutes_ms: number) {
+    const oneHourAgo = new Date(Date.now() - minutes_ms).toISOString()
     const list = await drive.files.list({
-        fields: "files(createdTime,id)",
+        fields: "files(createdTime,id,name)",
+        q: `'${folderID}' in parents and createdTime < '${oneHourAgo}'`,
     });
+    list.data.files!.forEach(file => {
+        drive.files.delete({
+            fileId: file.id!,
+        })
+    })
 }
-initialize()
